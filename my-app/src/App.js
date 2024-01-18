fb2cfc4c1c64eff0a898d12659975ea1908d8e11
Kulibars <125401672+Kulibars@users.noreply.github.com>
2024-01-18 15:05:57 +0300

    JS add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 // декларативный
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="Current-year">
-					<p>{new Date().getFullYear()}</p>
-				</div>
-			</header>
-		</div>
+	const currentYear = new Date().getFullYear();
+	return React.createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		React.createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement(
+				'p',
+				{
+					style: { fontSize: '50px' },
+					className: 'currentYear',
+				},
+				currentYear,
+			),
+		),
 	);
 };
